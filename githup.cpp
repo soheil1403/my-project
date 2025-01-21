c2b6d5fbf765c390dbfc253b1a242522c449c643
soheil1403 <lihesr1@gmail.com>
2025-01-21 22:01:59 +0330

    pro

--- githup.cpp
+++ githup.cpp
@@ -7,3 +7,21 @@ using namespace std;
 
 const int WIDTH = 10;  // عرض نقشه
 const int HEIGHT = 10; // ارتفاع نقشه
+char EMPTY = ' ';
+char PLAYER = 'S';
+char BOMB = 'B';
+char BRICK = 'Z';
+char CONCRETE = 'X';
+char ENEMY = 'E';
+char EXIT = 'G';  // دروازه خروج
+char BORDER = '*'; // مرز نقشه
+
+struct Bomb {
+    int x, y;
+    int moveCount;
+    bool active;
+};
+
+int playerX = 1, playerY = 1; // شروع بازیکن توی نقشه 
+vector<Bomb> bombs;
+char grid[HEIGHT + 2][WIDTH + 2]; // +2 برای مرزهای بالا و پایین
