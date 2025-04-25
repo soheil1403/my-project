e000f2936ab8a972a03c721f4e0e2db34d453368
soheil1403 <lihesr1@gmail.com>
2025-04-25 23:12:12 +0330

    hi

--- githup.cpp
+++ githup.cpp
@@ -13,4 +13,19 @@ class Reservation { private :
   Reservation (int res ,Status sta , time_t c , student* stu);//DiningHall* hall, Meal* m )                 
 
   void print() const ;
+  bool cancel ();
+
+  void setReservationID(int id);
+ // void setStudent(student* stu);
+ //void setDiningHall(DiningHall* hall);
+ //void setMeal(Meal* m);
+  void setStatus(Status stat);
+  void setCreatedAt(time_t c);
+
+
+  int getReservationID() const;
+  student* getStudent() const;
+  DiningHall* getDiningHall() const;
+  Meal* getMeal() const;
+ Status getStatus() const;
 
