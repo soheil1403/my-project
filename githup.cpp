59c5909e2c43317e53af557178baa4a1ed51b00e
soheil1403 <lihesr1@gmail.com>
2025-04-25 23:04:14 +0330

    hello

--- githup.cpp
+++ githup.cpp
@@ -2,23 +2,5 @@
 using namespace std;
 int main (){
 
-int num1 ,num2 ,num3 , temp;
-cin>>num1>>num2>>num3;
-if (num1 > num2) {
-     temp = num1;
-    num1 = num2;
-    num2 = temp;
-}
-  if (num2 > num3) {
-    temp = num2;
-    num2 = num3;
-    num3 = temp;
-}
-if (num1 > num2){
-    temp = num1;
-    num1 = num2;
-    num2 = temp;
+int num1 ,num2 ,num3 , temp;}
 
-
-}cout<<num1<<num2<<num3;
-}
